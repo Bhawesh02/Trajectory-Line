4a5592d87a2d585e4431b36d02b748f0b1e86b68
Bhawesh <bhaweshagarwal02@gmail.com>
2024-10-14 13:00:32 +0530

    Controls Created

--- Trajectory Line/Assets/Scripts/Cannon.cs
+++ Trajectory Line/Assets/Scripts/Cannon.cs
@@ -6,20 +6,39 @@ public class Cannon : MonoBehaviour {
     [SerializeField] private Ball m_ballPrefab;
     [SerializeField] private float m_force = 20;
     [SerializeField] private Transform m_ballSpawn;
+    [SerializeField] private float m_maxRotationAngle;
+    [SerializeField] private float m_rotationSpeed;
 
-    [SerializeField]private bool m_isMoved;
+    
+    private bool m_isMoved;
+    private float m_verticaAxisInput;
+    private float m_horizontalAxisInput;
+    private Vector3 m_turnAngle;
+    private float m_initialRotation;
 
     private void Start()
     {
         m_trajectoryProjection.SimulateTrajectory(m_ballPrefab, m_ballSpawn.position, m_ballSpawn.forward * m_force);
+        m_initialRotation = transform.localEulerAngles.y;
     }
 
     private void Update()
     {
-        //m_isMoved = false;
+        m_isMoved = false;
+        m_verticaAxisInput = Input.GetAxisRaw("Vertical");
+        m_horizontalAxisInput = Input.GetAxisRaw("Horizontal");
         HandelRotation();
         HandelMovement();
-       
+        FireBall();
+    }
+
+    private void FireBall()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Ball ball = Instantiate(m_ballPrefab, m_ballSpawn.position, Quaternion.identity);
+            ball.Init(m_ballSpawn.forward * m_force, false);
+        }
     }
 
     private void FixedUpdate()
@@ -32,11 +51,25 @@ public class Cannon : MonoBehaviour {
 
     private void HandelMovement()
     {
-      
+        if (Mathf.Approximately(m_verticaAxisInput, 0f))
+        {
+            return;
+        }
+        m_isMoved = true;
+        
     }
 
     private void HandelRotation()
     {
-      
+        if (Mathf.Approximately(m_horizontalAxisInput, 0f))
+        {
+            return;
+        }
+        m_isMoved = true;
+        m_turnAngle = transform.localEulerAngles;
+        m_turnAngle.y = Mathf.Lerp(m_turnAngle.y, 
+            m_maxRotationAngle * m_horizontalAxisInput + m_initialRotation,
+            m_rotationSpeed * Time.deltaTime);
+        transform.localEulerAngles = m_turnAngle;
     }
 }
